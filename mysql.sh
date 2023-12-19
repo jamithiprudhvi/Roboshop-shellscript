23b088d0e781789c15edd2233ccd447dac6c49c1
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-20 04:39:11 +0530

    adding shippinh & mysql

---
+++ mysql.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2....FAILED  $N"
+        exit 1
+    else
+        echo -e "$G $2 ....SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: RUN THIS SCRIPT WITH ROOT USER $N"
+    exit 1
+else
+    echo -e "$G YOU ARE A ROOT USER $N"
+fi
+
+dnf module disable mysql -y $LOGFILE
+
+VALIDATE $? "mysql disabling"
+
+cp /home/centos/Roboshop-shellscript/mysql.repo /etc/yum.repos.d/mysql.repo $LOGFILE
+
+VALIDATE $? "Copying files"
+
+dnf install mysql-community-server -y $LOGFILE
+
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld $LOGFILE
+
+VALIDATE $? "Enabling mysql"
+
+systemctl start mysqld $LOGFILE
+
+VALIDATE $? "starting mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1 $LOGFILE
+
+VALIDATE $? "Setting root password"
+
+mysql -uroot -pRoboShop@1 $LOGFILE
+
+VALIDATE $? "checking rootpassword"
---
+++ shipping.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2....FAILED  $N"
+        exit 1
+    else
+        echo -e "$G $2 ....SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: RUN THIS SCRIPT WITH ROOT USER $N"
+    exit 1
+else
+    echo -e "$G YOU ARE A ROOT USER $N"
+fi
+
+dnf install maven -y &>> $LOGFILE 
+
+VALIDATE $? "Installing maven"
+
+useradd roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop
+    VALIDATE $? "user roboshop created"
+else
+    echo -e "user already exists $Y .... SKIPPING $N"
+fi
+
+mkdir -p /app &>> $LOGFILE
+
+VALIDATE $? " creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading shipping application"
+
+cd /app &>> $LOGFILE
+
+unzip /tmp/shipping.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping shipping application"
+
+cd /app &>> $LOGFILE
+
+mvn clean package &>> $LOGFILE
+
+VALIDATE $? "cleaning mvn packages"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+
+VALIDATE $? "targeting shipping.jar"
+
+cp /home/centos/Roboshop-shellscript/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+
+VALIDATE $? "copying shipping .service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "Deamon reloading"
+
+systemctl enable shipping &>> $LOGFILE
+
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping &>> $LOGFILE
+
+VALIDATE $? "starting shipping"
+
+dnf install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing mysql"
+
+mysql -h mysql.prudhvi.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+
+VALIDATE $? "Loading schema"
+
+systemctl restart shipping &>> $LOGFILE
+
+VALIDATE $? "Restart shipping"
+
+
+
