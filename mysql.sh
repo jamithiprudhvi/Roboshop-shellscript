c8e2a62c2d174c6268ce7354e4920072a1f8099e
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-20 05:38:46 +0530

    adding shippinh & mysql

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 R="\e[31m"
-G="\e[32"
+G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
@@ -27,30 +27,30 @@ else
     echo -e "$G YOU ARE A ROOT USER $N"
 fi
 
-dnf module disable mysql -y $LOGFILE
+dnf module disable mysql -y &>>$LOGFILE
 
 VALIDATE $? "mysql disabling"
 
-cp /home/centos/Roboshop-shellscript/mysql.repo /etc/yum.repos.d/mysql.repo $LOGFILE
+cp /home/centos/Roboshop-shellscript/mysql.repo /etc/yum.repos.d/mysql.repo &>> $LOGFILE
 
 VALIDATE $? "Copying files"
 
-dnf install mysql-community-server -y $LOGFILE
+dnf install mysql-community-server -y &>> $LOGFILE
 
 VALIDATE $? "Installing mysql"
 
-systemctl enable mysqld $LOGFILE
+systemctl enable mysqld &>> $LOGFILE
 
 VALIDATE $? "Enabling mysql"
 
-systemctl start mysqld $LOGFILE
+systemctl start mysqld &>> $LOGFILE
 
 VALIDATE $? "starting mysql"
 
-mysql_secure_installation --set-root-pass RoboShop@1 $LOGFILE
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
 
 VALIDATE $? "Setting root password"
 
-mysql -uroot -pRoboShop@1 $LOGFILE
+mysql -uroot -pRoboShop@1 &>> $LOGFILE
 
 VALIDATE $? "checking rootpassword"
