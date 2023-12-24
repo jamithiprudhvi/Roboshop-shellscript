d2a1df0bd8d122ddeef67d99de39b199eb98e263
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-24 16:51:41 +0530

    remove roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-0d0ec1361a0648bef
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z081184230BU9L82VFSK3
+DOMAIN_NAME="prudhvi.online"
 for i in "${INSTANCES[@]}"
 do
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
@@ -13,5 +15,25 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-        echo "$i: $IP_ADDRESS"
+    echo "$i: $IP_ADDRESS"
+
+  #Create aws route53 record make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch "
+    {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "$i.$DOMAIN_NAME"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "$IP_ADDRESS"
+            }]
+        }
+        }]
+    }
+    "
 done
