0116ebc6db8d88ad420bc5e5e8196bc32ce03f02
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-24 17:17:50 +0530

    remove roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -19,21 +19,21 @@ do
 
   #Create aws route53 record make sure you delete existing record
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch "
+    --hosted-zone-id $zoneid \
+    --change-batch '
     {
-        "Comment": "Testing creating a record set"
+        "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "$i.$DOMAIN_NAME"
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "$IP_ADDRESS"
+                "Value"         : "'$IP_ADDRESS'"
             }]
         }
         }]
     }
-    "
+        '
 done
