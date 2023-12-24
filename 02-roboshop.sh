311dc650346ae3318d9ba3b416882b182f02639a
prudhvi <jamithireddyprudhvi9395@gmail.com>
2023-12-24 14:30:58 +0530

    remove roboshop

--- 02-roboshop.sh
+++ 02-roboshop.sh
@@ -4,6 +4,7 @@ AMI=ami-03265a0778a880afb
 SG_ID=sg-0d0ec1361a0648bef
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
+
 for i in "${INSTANCES[@]}"
 do 
     echo "instance is: $i"
@@ -14,5 +15,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    D=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]")
 done
